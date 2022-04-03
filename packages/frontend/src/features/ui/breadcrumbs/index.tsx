24d918a319d1790d48108ef223fc73cf2e028b4a
Evgeniy Talko <etalko@gmail.com>
2022-04-03 21:18:33 +0300

    feat(frontend): add breadcrumbs

---
+++ packages/frontend/src/features/ui/breadcrumbs/index.tsx
@@ -0,0 +1,29 @@
+import { Breadcrumb } from 'antd'
+import { Link } from 'react-router-dom'
+
+import { routesCollection as routes } from 'shared/config/routes'
+
+const Breadcrumbs = () => {
+    // TODO: implement changing on navigation
+    // TODO: add location path reading
+    // https://ant.design/components/breadcrumb/
+    return (
+        <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>
+                <Link to={routes.home.basePath}>{routes.home.name}</Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>
+                <Link to={routes.experimentsList.basePath}>
+                    {routes.experimentsList.name}
+                </Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>
+                <Link to={routes.experiment.basePath}>
+                    {routes.experiment.name}
+                </Link>
+            </Breadcrumb.Item>
+        </Breadcrumb>
+    )
+}
+
+export { Breadcrumbs }
