8805f1bc62bf0f5542d2e24a2a39e73e6a2a5898
Evgeniy Talko <etalko@gmail.com>
2022-04-03 21:15:05 +0300

    WIP(layout): update layout with common elements

--- packages/frontend/src/widgets/LayoutMain/index.tsx
+++ packages/frontend/src/widgets/LayoutMain/index.tsx
@@ -1,60 +1,109 @@
 import { Layout, Menu } from 'antd'
+import { useState } from 'react'
+import { AiOutlineHeatMap, AiOutlinePoweroff } from 'react-icons/ai'
+import { BsThermometerHalf } from 'react-icons/bs'
+import { GiElectric } from 'react-icons/gi'
 import { Link, Outlet } from 'react-router-dom'
 
 import { routesCollection as routes } from 'shared/config/routes'
 
 import { Breadcrumbs } from 'features'
 
-const { Header, Content } = Layout
+const { Header, Content, Sider } = Layout
 
 function LayoutMain() {
+    const [asideCollapsed, setAsideCollapsed] = useState(false)
+
     return (
-        <Layout style={{ height: '100vh' }}>
-            <Header
-                style={{
-                    position: 'fixed',
-                    zIndex: 1,
-                    width: '100%',
-                    display: 'flex',
-                    alignItems: 'center',
-                }}
-            >
-                <Menu
-                    theme="dark"
-                    mode="horizontal"
-                    defaultSelectedKeys={['1']}
+        <Layout>
+            <Layout style={{ height: '100vh' }}>
+                <Header
+                    style={{
+                        position: 'fixed',
+                        zIndex: 1,
+                        width: '100%',
+                        display: 'flex',
+                        alignItems: 'center',
+                    }}
                 >
-                    <Menu.Item key="1">
-                        <Link to={routes.home.basePath}>
-                            {routes.home.name}
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item key="2">
-                        <Link to={routes.experimentsList.basePath}>
-                            {routes.experimentsList.name}
-                        </Link>
-                    </Menu.Item>
-                    {/* Появляется только после старта эксперимента */}
-                    <Menu.Item key="3">
-                        <Link to={routes.experiment.basePath}>
-                            Текущий эксперимент
-                        </Link>
-                    </Menu.Item>
-                </Menu>
-                {/* TODO: Start experiment button which navigates to a currently started experiment */}
-                {/* TODO: device metadata */}
-            </Header>
-            <Content
-                style={{
-                    padding: '0 50px',
-                    marginTop: 64,
-                    backgroundColor: '#ffffff',
-                }}
-            >
-                <Breadcrumbs />
-                <Outlet />
-            </Content>
-            {/* TODO: aside with controls */}
+                    <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={['1']}
+                    >
+                        <Menu.Item key="1">
+                            <Link to={routes.home.basePath}>
+                                {routes.home.name}
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Link to={routes.experimentsList.basePath}>
+                                {routes.experimentsList.name}
+                            </Link>
+                        </Menu.Item>
+                        {/* Появляется только после старта эксперимента */}
+                        <Menu.Item key="3">
+                            <Link to={routes.experiment.basePath}>
+                                Текущий эксперимент
+                            </Link>
+                        </Menu.Item>
+                    </Menu>
+                    {/* TODO: Start experiment button which navigates to a currently started experiment */}
+                    {/* TODO: device metadata */}
+                </Header>
+                <Content
+                    style={{
+                        padding: '0 50px',
+                        marginTop: 64,
+                        backgroundColor: '#ffffff',
+                    }}
+                >
+                    <Breadcrumbs />
+                    <Outlet />
+                </Content>
+                <Sider
+                    collapsible
+                    collapsed={asideCollapsed}
+                    onCollapse={setAsideCollapsed}
+                    reverseArrow={true}
+                    theme="light"
+                    style={{
+                        borderLeft: '1px solid #e8e8e8',
+                        boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+                    }}
+                >
+                    <div
+                        className="logo"
+                        style={{
+                            height: '32px',
+                            backgroundColor: '#eeeeee',
+                            margin: '16px',
+                        }}
+                    />
+                    {/* TODO: buld device stats panel*/}
+                    <Menu>
+                        <Menu.Item key="model" icon={<AiOutlineHeatMap />}>
+                            JY2000-2
+                        </Menu.Item>
+                        <Menu.Item
+                            key="power-state"
+                            icon={<AiOutlinePoweroff />}
+                        >
+                            (вкл/выкл)
+                        </Menu.Item>
+                        <Menu.Item key="voltage-state" icon={<GiElectric />}>
+                            11В
+                        </Menu.Item>
+                        <Menu.Item
+                            key="heat-state"
+                            icon={<BsThermometerHalf />}
+                            title="40°C"
+                        >
+                            40°C
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+            </Layout>
         </Layout>
     )
 }
