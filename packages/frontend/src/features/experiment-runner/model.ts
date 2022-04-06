2c3ac5566d10edc55a97a40e6f9859e3396b477d
Evgeniy Talko <etalko@gmail.com>
2022-04-06 12:34:37 +0300

    style(frontend): rm comments

--- packages/frontend/src/features/experiment-runner/model.ts
+++ packages/frontend/src/features/experiment-runner/model.ts
@@ -12,6 +12,7 @@ const status = atom<ExperimentStatus>({
         },
     ],
 })
+
 const isStarted = selector({
     key: 'isStarted',
     get: ({ get }) => {
@@ -19,30 +20,5 @@ const isStarted = selector({
         return state === 'idle' || state === 'stopped' ? false : true
     },
 })
-
-// const setStart = action(status, 'start', (status) => status.set('started'))
-
-// const setStop = action(status, 'stop', (status) => status.set('stopped'))
-
-// const isStarted = computed(status, (status) =>
-//     status === 'idle' || status === 'stopped' ? false : true
-// )
-
-// const toggleStatus = action(status, 'toggle', (started) => {
-//     if (started.get() === 'idle' || started.get() === 'stopped') {
-//         setStart()
-//     } else {
-//         setStop()
-//     }
-// })
-
-// // subscribe to the status changes and emit to WebSocket
-// status.listen((status) => {
-//     if (status === 'started') {
-//         console.log('started')
-//     } else {
-//         console.log('stopped')
-//     }
-// })
 
 export { status, isStarted }
