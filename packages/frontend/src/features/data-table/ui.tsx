c5a4ea7bd7eea5d902f9a5455fd7a92e3f068086
Evgeniy Talko <etalko@gmail.com>
2022-04-06 17:52:53 +0300

    feat(frontend): add data-table

---
+++ packages/frontend/src/features/data-table/ui.tsx
@@ -0,0 +1,34 @@
+import { Table } from 'antd'
+
+import { WebsocketMessage } from '@app/types'
+
+type Props = {
+    children?: React.ReactNode
+    dataset: WebsocketMessage['payload'][] | []
+}
+
+function DataTable({ dataset = [] }: Props) {
+    const columns = [
+        {
+            title: 'id',
+            dataIndex: 'sensorId',
+            key: 'sensorId',
+            fixed: true,
+            width: 150,
+        },
+        { title: 'Time', dataIndex: 'timestamp', key: 'timestamp' },
+        { title: 'Value', dataIndex: 'value', key: 'value' },
+    ]
+
+    return (
+        <Table
+            bordered
+            // @ts-expect-error
+            dataSource={dataset}
+            columns={columns}
+            scroll={{ x: 500 }}
+        />
+    )
+}
+
+export { DataTable }
