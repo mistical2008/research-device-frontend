f2be01aec79b42cbdb8cb2fd29639a2cf5d53b9d
Evgeniy Talko <etalko@gmail.com>
2022-04-07 17:29:31 +0300

    fix: .env backend port for backend and frontend

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -16,9 +16,9 @@ import {
 } from './lib/utils'
 import 'dotenv/config'
 import { nanoid } from 'nanoid'
-import { NanoId, SocketIOType } from '@app/types'
+import { NanoId, SocketIOServerType } from '@app/types'
 
-const port = process.env.BACKEND_PORT || 3000
+const port = process.env.BACKEND_PORT || 7777
 const server = fastify()
 const genRandFload10to100 = genRandFloat(10, 100)
 const intervals = [10, 200]
@@ -33,7 +33,7 @@ server.register(fastifyIO, {
     },
 })
 
-function sendSensorsData(socket: SocketIOType, sensorId: NanoId): void {
+function sendSensorsData(socket: SocketIOServerType, sensorId: NanoId): void {
     emitData(socket, 'message', {
         payload: {
             sensorId,
